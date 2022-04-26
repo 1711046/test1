c8aa73ff1179b4e4098ca52ad4718660ffd95fee
1711046 <104405375+1711046@users.noreply.github.com>
2022-04-26 10:18:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-	
+    sprite.delete()
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    sprite.delete()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    sprite.delete()
 })
-basic.forever(function () {
-	
-})
+let sprite: game.LedSprite = null
+music.playMelody("B B C5 A F B G G ", 110)
+basic.showString("321")
+for (let index = 0; index < 5; index++) {
+    sprite = game.createSprite(randint(0, 4), randint(0, 4))
+    basic.pause(1000)
+}
+for (let index = 0; index < 8; index++) {
+    sprite = game.createSprite(randint(0, 4), randint(0, 4))
+    basic.pause(700)
+}
+for (let index = 0; index < 12; index++) {
+    sprite = game.createSprite(randint(0, 4), randint(0, 4))
+    basic.pause(500)
+}
+for (let index = 0; index < 24; index++) {
+    sprite = game.createSprite(randint(0, 4), randint(0, 4))
+    basic.pause(350)
+}
+basic.clearScreen()
+basic.showLeds(`
+    # # # # #
+    . # # # .
+    . # # # .
+    . . # . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    . # # # .
+    . # # # .
+    . . # . .
+    # # # # #
+    `)
